e18b2c44d4760e5a9c152e005e7f271f3db6d51f
Cassiel0925 <1730604282@qq.com>
2021-12-10 15:12:20 +0800

    https://github.com/Cassiel0925/tabbar.git

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,19 @@
   <div id="app">
     <router-view></router-view>
     <!-- 3. 使用 -->
-    <tab-bar>
-      <tab-bar-item path="/home">
-        <i class="icon-home" slot="item-icon"></i>
-        <i class="icon-home" slot="item-icon-active" style="color: pink"></i>
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item path="/category">
-        <i class="icon-grid" slot="item-icon"></i>
-        <i class="icon-grid" slot="item-icon-active" style="color: pink"></i>
-        <div slot="item-text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item path="/cart">
-        <i class="icon-cart" slot="item-icon"></i>
-        <i class="icon-cart" slot="item-icon-active" style="color: pink"></i>
-        <div slot="item-text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item path="/profile">
-        <i class="icon-user" slot="item-icon"></i>
-        <i class="icon-user" slot="item-icon-active" style="color: pink"></i>
-        <div slot="item-text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-// 1. 导入组件
-import TabBar from './components/tabbar/TabBar.vue'
-import TabBarItem from './components/tabbar/TabBarItem.vue'
+import MainTabBar from './components/mainTabBar/MainTabBar.vue'
 
 export default {
   name: 'App',
-  // 2. 注册组件
   components: {
-    TabBar,
-    TabBarItem
+    MainTabBar
   }
+  
 }
 </script>
 
---
+++ src/components/mainTabBar/MainTabBar.vue
@@ -0,0 +1,43 @@
+.<template>
+  <tab-bar>
+      <tab-bar-item path="/home">
+        <i class="icon-home" slot="item-icon"></i>
+        <i class="icon-home" slot="item-icon-active" style="color: pink"></i>
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <i class="icon-grid" slot="item-icon"></i>
+        <i class="icon-grid" slot="item-icon-active" style="color: pink"></i>
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <i class="icon-cart" slot="item-icon"></i>
+        <i class="icon-cart" slot="item-icon-active" style="color: pink"></i>
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <i class="icon-user" slot="item-icon"></i>
+        <i class="icon-user" slot="item-icon-active" style="color: pink"></i>
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+// 1. 导入组件
+import TabBar from '../tabbar/TabBar.vue'
+import TabBarItem from '../tabbar/TabBarItem.vue'
+
+export default {
+    name: 'MainTabBar',
+    // 2. 注册组件
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
