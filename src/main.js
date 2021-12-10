53bd815fe379405fbc26a9c1a3901be3a7595dad
Cassiel0925 <1730604282@qq.com>
2021-12-10 11:04:12 +0800

    https://github.com/Cassiel0925/tabbar.git

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
+
+// require('./assets/css/base.css')
